fc3b086754ca5c8ff30ea961de22f751e0e771d7
Ignacio Nin <nacho@vividcortex.net>
2014-11-14 16:19:26 -0200

    Puntapie inicial

---
+++ sorteo.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+program="$(basename "$0")"
+
+reset_list() {
+    cp list curr 
+    usage
+}
+
+count() {
+    wc -l $@ | awk '{ print $1 }'
+}
+
+usage() {
+    echo >&2 "List ready ($(count curr) regs)"
+    echo >&2 "    run \`$program draw [n]' to draw results"
+    echo >&2 "    run \`$program reset' to reset list"
+}
+
+confirm() {
+    local msg
+    local n rnd
+    local var
+
+    msg="$1"
+    n="$(expr $RANDOM \* 100 + $RANDOM)"
+    n="$(expr $n % "$(count /usr/share/dict/words )" + 1)"
+    rnd="$(head -n "$n" /usr/share/dict/words | tail -1)"
+
+    printf >&2 "%s. To confirm type \`%s': " "$msg" "$rnd"
+    read var
+    if test "x$rnd" == "x$var"
+    then
+        return 0
+    fi
+
+    return 1
+
+}
+
+draw() {
+    local draw i
+    local n rnd
+
+    draw="$1"
+
+    for i in $(seq 1 $draw)
+    do
+        n="$(expr $RANDOM % "$(count curr)" + 1)"
+        rnd="$(head -n "$n" curr | tail -n 1)"
+        echo "$rnd" | awk '{ print "WINRAR: " $2 " " $1 }'
+    done
+
+}
+
+if test "$#" -ge 1
+then
+    command="$1"
+fi
+
+# Check that the link exists
+if ! test -r curr
+then
+    # Check attendee list
+    if ! test -r list
+    then
+        echo >&2 "No source list."
+        exit 2
+    fi
+
+    if test "$command" != "reset"
+    then
+        echo >&2 "No current list: run \`$program reset' to start"
+        exit 2
+    fi
+
+    exit 0
+
+fi
+
+case "$command" in
+    reset )
+        if confirm "Current list exists"
+        then
+            reset_list
+        else
+            echo >&2 "List not reset"
+        fi
+
+        exit 0
+        ;;
+    draw )
+        if test "$#" -ge 2
+        then
+            draw="$2";
+        else
+            draw=1
+        fi
+
+        if ! echo "$draw" | grep -q ^[0-9][0-9]*$
+        then
+            echo >&2 "draw: Incorrect number format \`$draw'!"
+            exit 1
+        fi
+
+        draw $draw
+        ;;
+    * )
+        echo >&2 "Unknown command \`$command'"
+        usage
+        exit 0
+        ;;
+esac
+
